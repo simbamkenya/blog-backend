85ef8c5cbdaa32cec8ea7ae409e4ca5b057e0541
simbankenya <pharesmuruthi@gmail.com>
2023-02-07 11:21:58 +0300

    handled category crud

--- routers/Categories.js
+++ routers/Categories.js
@@ -1,8 +1,22 @@
 const { Category } = require('../models/Category');
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const { route } = require('./Posts');
 
+
+
+
+//posting a category 
+router.post('/', async (req, res) => {
+    const { name } = req.body;
+
+    let category = new Category({ name });
+    category = await category.save()
+    if(!category) return res.status(400).send('the category was not created')
+    
+    res.send(category)
+})
 
 //fetch categories
 router.get('/', async (req, res) => {
@@ -18,14 +32,12 @@ router.get('/:id', async (req, res) => {
 
     res.status(200).send(category)
 })
-//update a category
-router.put('/:id', async (req, res) => {
-  const category = await Category.find
-})
 
 //deleted a category
 router.delete('/:id', async (req, res) => {
-
+   const category = await Category.findByIdAndRemove(req.params.id)
+   if(!category) return res.status(404).send('users not deleted')
+   res.status(200).send(category)
 })
 
 
